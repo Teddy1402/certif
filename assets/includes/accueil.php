a0cefb9ccf9ac0c57089efaf3bd00a0c87eaa2af
Teddy1402 <teddy.008@outlook.com>
2022-06-07 16:28:08 +0200

    accueil

---
+++ assets/includes/accueil.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.lordicon.com/xdjxvujz.js"></script>
+</head>
+<body>
+    
+    <div class="container-accueil">
+        <div class="titre-accueil">
+            <ul>
+                <li><h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1></li>
+                <li><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo explicabo et ex consectetur id, ea atque quam ipsum provident qui consequatur ratione porro error sequi nulla dicta ab adipisci veritatis.</p></li>
+            </ul>
+        </div>
+        <div class="image-accueil"></div>
+    </div>
+
+    <div class="container-icone">
+        <div class="icone-1">
+            <ul>
+                <li><lord-icon
+                    src="https://cdn.lordicon.com/wxnxiano.json"
+                    trigger="loop-on-hover"
+                    delay="2000"
+                    style="width:250px;height:250px">    
+                </lord-icon>
+                </li>
+                <script src="https://cdn.lordicon.com/xdjxvujz.js"></script>
+
+                <li><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p></li>
+            </ul>
+        </div>
+        <div class="icone-1">
+            <ul>
+                <li>
+                    <lord-icon
+                        src="https://cdn.lordicon.com/wxnxiano.json"
+                        trigger="loop-on-hover"
+                        delay="2000"
+                        style="width:250px;height:250px">
+                    </lord-icon>
+                </li>
+                <li><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p></li>
+            </ul>
+        </div>
+    </div>
+
+
+    <h2>Projet</h2>
+
+    <div class="container-projet">
+        <div class="projet-1">
+            <ul>
+                <li><img src="assets/images/Capture.PNG" alt=""></li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+        <div class="projet-1">
+            <ul>
+                <li>image</li>
+                <li>titre</li>
+                <li>texte</li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+</html>
